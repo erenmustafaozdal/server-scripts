ccdd5bc32f52d945a4632ae337bfbfaf5dc97bd7
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2017-07-02 21:49:39 +0100

    satır sona gelmeden yarıda kalması sorunu çözüldü

--- general.sh
+++ general.sh
@@ -322,11 +322,11 @@ function job_color()
 # Now we construct the prompt. PROMPT_COMMAND="history -a"
 case ${TERM} in
     *term | rxvt | linux)
-        PS1="\[\$(disk_color)\][\w]\[${NC}\]\n"                                 # PWD (with 'disk space' info)
-        PS1=${PS1}"${Gray}(\[\$(load_color)\]\A${Gray})\[${NC}\]"               # Time of day (with load info)
-        PS1=${PS1}"${Gray}-(\[${SU}\]\u\[${NC}\]@\[${CNX}\]\h${Gray})\[${NC}\]" # User@Host (with connection type info)
-        PS1=${PS1}"\[\$(job_color)\]->\[${NC}\] "                               # Prompt (with 'job' info)
-        PS1=${PS1}"\[\e]0;(\A)-(\u@\h) -> \w\a"                                 # Set title of current xterm
+        PS1="\[$(disk_color)\][\w]\[${NC}\]\n"                                          # PWD (with 'disk space' info)
+        PS1=${PS1}"\[${Gray}\](\[$(load_color)\]\A\[${Gray}\])\[${NC}\]"                # Time of day (with load info)
+        PS1=${PS1}"\[${Gray}\]-(\[${SU}\]\u\[${NC}\]@\[${CNX}\]\h\[${Gray}\])\[${NC}\]" # User@Host (with connection type info)
+        PS1=${PS1}"\[$(job_color)\]->\[${NC}\] "                                        # Prompt (with 'job' info)
+        PS1=${PS1}"\[\e]0;(\A)-(\u@\h) -> \w\a"                                         # Set title of current xterm
         ;;
     *)
         PS1="(\A \u@\h \W) > "                                  # --> PS1="(\A \u@\h \w) > "
